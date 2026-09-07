55879189e9997779931980e336ae2f63a9aa075d
Euneak <euneakcr7@gmail.com>
2026-09-07 10:18:06 +0530

    Configure Jenkins CI pipeline

--- src/main/java/Base/BaseClass.java
+++ src/main/java/Base/BaseClass.java
@@ -3,6 +3,7 @@ package Base;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
@@ -12,6 +13,19 @@ public class BaseClass {
         @BeforeSuite
         public void launchBrowser() {
             WebDriverManager.chromedriver().setup();
+// Chrome configuration
+            ChromeOptions options = new ChromeOptions();
+
+            // Run Chrome without opening a visible browser window
+            options.addArguments("--headless=new");
+
+            // Give Chrome a fixed screen size
+            options.addArguments("--window-size=1920,1080");
+
+            // Useful when running Chrome in Jenkins/Linux
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+
             driver = new ChromeDriver();
             driver.manage().window().maximize();
             driver.get("https://stage.fourplus.net");
