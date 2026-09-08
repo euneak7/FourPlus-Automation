f0f88a3ec8709a33e1330b02d16599fa1613d183
Euneak <euneakcr7@gmail.com>
2026-09-08 13:18:03 +0530

    Test GitHub webhook

--- src/main/java/Utilities/ExtentReportManager.java
+++ src/main/java/Utilities/ExtentReportManager.java
@@ -1,4 +1,34 @@
 package Utilities;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
 public class ExtentReportManager {
+    public static ExtentReports extent;
+    public static ExtentTest test;
+    public static void startReport(){
+        ExtentSparkReporter spark = new ExtentSparkReporter("test-output/ExtentReport.html");
+        extent = new ExtentReports();
+        extent.attachReporter(spark);
+        extent.setSystemInfo("Project", "FourPlus Automation");
+        extent.setSystemInfo("Tester", "Euneak");
+        extent.setSystemInfo("Environment", "Stage");
+        extent.setSystemInfo("Browser", "Chrome");
+    }
+    // Create a test
+    public static void createTest(String testName) {
+
+        test = extent.createTest(testName);
+    }
+
+    // End the report
+    public static void endReport() {
+
+        extent.flush();
+    }
 }
+
+
+
+
--- src/test/java/Test/AdminPanelTest.java
+++ src/test/java/Test/AdminPanelTest.java
@@ -2,41 +2,57 @@ package Test;
 
 import Base.BaseClass;
 import Pages.*;
+import Utilities.ExtentReportManager;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 public class AdminPanelTest extends BaseClass {
+    @BeforeSuite
+    public void startExtentReport() {
+
+        ExtentReportManager.startReport();
+    }
+
 
     @Test
     public void verifyAdminModules() throws InterruptedException {
+        ExtentReportManager.createTest("Verify Admin Modules");
 
+        ExtentReportManager.test.info("Admin panel automation started");
         // 1. Login
         LoginPage loginPage = new LoginPage(driver);
 
         loginPage.login("admin@gmail.com", "Admin@12345");
+        ExtentReportManager.test.pass("Login completed successfully");
         Thread.sleep(3000);
 
         // 2. Category
         CategoryPage categoryPage = new CategoryPage(driver);
 
         categoryPage.clickCategory();
+        ExtentReportManager.test.pass("Category clicked successfully");
         Thread.sleep(3000);
 
         // 3. Product
         ProductPage productPage = new ProductPage(driver);
 
         productPage.clickProductPage();
+        ExtentReportManager.test.pass("Product page clicked successfully");
         Thread.sleep(3000);
 
         // 4. Orders
         OrdersPage ordersPage = new OrdersPage(driver);
 
         ordersPage.ClickOrdersPage();
+        ExtentReportManager.test.pass("Orders page clicked successfully");
         Thread.sleep(3000);
         // 5. Banner
         BannerManagementPage bannerPage =
                 new BannerManagementPage(driver);
 
         bannerPage.ClickBannerManagementPage();
+        ExtentReportManager.test.pass("Banner management page clicked successfully");
         Thread.sleep(3000);
 
         // 6. Bank Management
@@ -44,14 +60,14 @@ public class AdminPanelTest extends BaseClass {
                 new BankManagementPage(driver);
 
         bankPage.ClickBankManagementPage();
-
+        ExtentReportManager.test.pass("Bank management page clicked successfully");
         Thread.sleep(3000);
         // 7. User Management
         UserManagementPage userPage =
                 new UserManagementPage(driver);
 
         userPage.ClickUserManagementPage();
-
+        ExtentReportManager.test.pass("User management page clicked successfully");
         Thread.sleep(3000);
 
         // 8. Settings
@@ -59,7 +75,13 @@ public class AdminPanelTest extends BaseClass {
                 new SettingsPage(driver);
 
         settingsPage.ClickSettingsPage();
+        ExtentReportManager.test.pass("Settings page clicked successfully");
         Thread.sleep(3000);
+    }
 
+        @AfterSuite
+        public void endExtentReport() {
+
+            ExtentReportManager.endReport();
     }
 }
